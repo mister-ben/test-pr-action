a1326c58d0931018045d13b43edaddd37e346366
mister-ben <git@misterben.me>
2024-02-27 16:18:36 +0100

    remove checkout action

--- .github/actions/pr-titles.js
+++ .github/actions/pr-titles.js
@@ -1,7 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-
 (async function run() {
   const title = github.context.payload.pull_request?.title;
   const titleRegex = /^(chore|ci|docs|feat|fix|refactor|revert|test)(\(.+\))?!?: (.+)/;
@@ -9,6 +8,6 @@ const github = require('@actions/github');
   if (!!title.match(titleRegex)) {
     core.info('PR title OK');
   } else {
-    core.setFailed('Please use conventional commit style for the PR title, so the ,erged change appears in the changelog. See https://www.conventionalcommits.org/.');
+    core.setFailed('Please use conventional commit style for the PR title, so the merged change appears in the changelog. See https://www.conventionalcommits.org/.');
   }
 })();
